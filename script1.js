198d5da30711f00f0a121d636620fc203de9dd5c
GustavoPercastre <angelpercastre293@gmail.com>
2021-11-09 23:44:06 -0800

    Created the loop that looks like a triangle for script1.js.

--- script1.js
+++ script1.js
@@ -7,3 +7,24 @@
 // ###
 // ##
 // #
+let n = 4;
+
+let string = "";
+
+for(let i = 1; i <= n; i++) 
+{
+  for(let j = 0; j < i; j++) 
+  {
+    string += "#";
+  }
+  string += "\n";
+}
+for(let i = 1; i <= n - 1; i++) 
+{
+  for (let j = 0; j < n - i; j++) 
+  {
+    string += "#";
+  }
+  string += "\n";
+}
+console.log(string);
